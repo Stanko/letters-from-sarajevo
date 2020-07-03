432730ba019a1057f534bbf87946b0cb7cc95d57
Stanko <stanko.tadic@gmail.com>
2020-07-03 12:45:35 +0200

    Added errors to the bash scripts

--- build-serbian.sh
+++ build-serbian.sh
@@ -9,13 +9,25 @@ cp ./sr/404.html ./404.html
 cp ./sr/_config.yml ./_config.yml
 
 # remove old build
-npm run clean
+if ! npm run clean
+then
+  echo ERROR: build failed
+  exit 1 # terminate and indicate error
+fi
 
 # build assets
-npm run build-assets
+if ! npm run build-assets
+then
+  echo ERROR: build failed
+  exit 2 # terminate and indicate error
+fi
 
 # build jekyll
-npm run build-jekyll
+if ! npm run build-jekyll
+then
+  echo ERROR: build failed
+  exit 3 # terminate and indicate error
+fi
 
 # create redirects for other languages
 mkdir ./_site/en
--- build.sh
+++ build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 # remove old build
-npm run clean
+if ! npm run clean
+then
+  echo ERROR: build failed
+  exit 1 # terminate and indicate error
+fi
 
 # build assets
-npm run build-assets
+if ! npm run build-assets
+then
+  echo ERROR: build failed
+  exit 2 # terminate and indicate error
+fi
 
 # build jekyll
-npm run build-jekyll
+if ! npm run build-jekyll
+then
+  echo ERROR: build failed
+  exit 3 # terminate and indicate error
+fi
 
 # create redirect for serbian version
 mkdir ./_site/sr
